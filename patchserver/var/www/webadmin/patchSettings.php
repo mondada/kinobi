0f06f7a3eb6676de53b97ffcb9a03c2a571bd45e
Duncan McCracken <dcmccracken@me.com>
2018-06-26 20:01:26 +1000

    Settings width
    Updated to align with NetSUS 4.5

--- patchserver/var/www/webadmin/patchSettings.php
+++ patchserver/var/www/webadmin/patchSettings.php
@@ -179,7 +179,7 @@ $(document).ready(function() {
 <h2>Patch</h2>
 
 <div class="row">
-	<div class="col-sm-12 col-md-9"> 
+	<div class="col-xs-12">
 
 		<form action="patchSettings.php" method="post" name="Database" id="Database" enctype="multipart/form-data">
 
