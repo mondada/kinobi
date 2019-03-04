573c2dc38f781689d25359667fd19cf3b0cf5eda
Duncan McCracken <dcmccracken@me.com>
2019-03-05 08:49:56 +1100

    Update index.php
    Added database check

--- patchserver/var/www/webadmin/index.php
+++ patchserver/var/www/webadmin/index.php
@@ -15,7 +15,45 @@
 if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
 	header("Location: https://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
 }
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta http-equiv="pragma" content="no-cache">
+		<meta http-equiv="expires" content="0">
+
+		<title><?php echo (isset($title) ? $title : "Kinobi"); ?></title>
+
+		<!-- Bootstrap -->
+		<link href="theme/bootstrap.css" rel="stylesheet" media="all">
+
+		<!-- Roboto Font -->
+		<link href="theme/roboto.font.css" rel="stylesheet" type="text/css">
+
+		<!-- Custom styles for this project -->
+		<link href="theme/custom.css" rel="stylesheet" type="text/css">
+
+		<!-- JQuery -->
+		<script type="text/javascript" src="scripts/jquery/jquery-2.2.4.js"></script>
+
+		<!-- Bootstrap JavaScript -->
+		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+	</head>
+
+	<style>
+		body {
+			background-color: #292929;
+		}
+	</style>
+
+	<body>
+
+<?php
 session_start();
 // Settings
 if (file_exists("inc/config.php")) {
@@ -28,70 +66,52 @@ include "inc/patch/database.php";
 
 $isAuth = false;
 
-$amAuthURL = "patchTitles.php";
-if (isset($_SESSION['isAuthUser'])) {
-	header("Location: ".$amAuthURL);
-}
+if (!$pdo) { ?>
+		<div style="margin-top: 11px; margin-bottom: 16px; border-color: #d43f3a;" class="panel panel-danger">
+			<div class="panel-body">
+				<div class="text-muted"><span class="text-danger glyphicon glyphicon-exclamation-sign" style="padding-right: 12px;"></span><?php echo $pdo_error; ?></div>
+			</div>
+		</div>
+<?php } else {
+	$amAuthURL = "patchTitles.php";
+	if (isset($_SESSION['isAuthUser'])) {
+		header("Location: ".$amAuthURL);
+	}
 
-if ((isset($_POST['username'])) && (isset($_POST['password']))) {
-	$username = $_POST['username'];
-	$_SESSION['username'] = $username;
+	if ((isset($_POST['username'])) && (isset($_POST['password']))) {
+		$username = $_POST['username'];
+		$_SESSION['username'] = $username;
 
-	$password = hash("sha256", $_POST['password']);
+		$password = hash("sha256", $_POST['password']);
 
-	if (($username != "") && ($password != "")) {
-		$users = getSettingUsers($pdo);
-		$isAuth = (array_key_exists($username, $users) ? $users[$username]['password'] == $password : false);
-		if ($isAuth) {
-			$isAuth = (isset($users[$username]['web']) ? $users[$username]['web'] : false);
+		if (($username != "") && ($password != "")) {
+			$users = getSettingUsers($pdo);
+			$isAuth = (array_key_exists($username, $users) ? $users[$username]['password'] == $password : false);
 			if ($isAuth) {
-				$isAuth = (isset($users[$username]['expires']) ? $users[$username]['expires'] > time() : true);
-				$loginerror = ($isAuth ? null : "User Account Expired");
+				$isAuth = (isset($users[$username]['web']) ? $users[$username]['web'] : false);
+				if ($isAuth) {
+					$isAuth = (isset($users[$username]['expires']) ? $users[$username]['expires'] > time() : true);
+					$loginerror = ($isAuth ? null : "User Account Expired");
+				} else {
+					$loginerror = "Access Denied.";
+				}
 			} else {
-				$loginerror = "Access Denied.";
+				$loginerror = "Invalid Username or Password.";
 			}
-		} else {
-			$loginerror = "Invalid Username or Password.";
 		}
 	}
-}
 
-if ($isAuth) {
-	$_SESSION['isAuthUser'] = 1;
-	$sURL = "patchTitles.php";
-	header("Location: ". $sURL);
-}
+	if ($isAuth) {
+		$_SESSION['isAuthUser'] = 1;
+		$sURL = "patchTitles.php";
+		header("Location: ". $sURL);
+	}
 ?>
-<!DOCTYPE html>
-
-<html>
-	<head>
-		<title>Kinobi Login</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-		<meta http-equiv="expires" content="0">
-		<meta http-equiv="pragma" content="no-cache">
-		<!-- Roboto Font CSS -->
-		<link href="theme/roboto.font.css" rel="stylesheet" type="text/css">
-		<!-- Bootstrap CSS -->
-		<link href="theme/bootstrap.css" rel="stylesheet" media="all">
-		<!-- Project CSS -->
-		<link rel="stylesheet" href="theme/custom.css" type="text/css">
-		<style>
-			body {
-				background-color: #292929;
-			}
-		</style>
-		<script type="text/javascript" src="scripts/jquery/jquery-2.2.4.js"></script>
-		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
-
 		<script>
 			$(window).load(function() {
 				$('#login-modal').modal('show');
 			});
 		</script>
-	</head>
-
-	<body>
 
 		<form name="loginForm" class="form-horizontal" id="login-form" method="post">
 			<!-- Delete Title Modal -->
@@ -124,5 +144,6 @@ if ($isAuth) {
 			<!-- /.modal -->
 		</form>
 
+<?php } ?>
 	</body>
 </html>
