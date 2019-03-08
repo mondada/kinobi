4ac39d00d72fbf62d23a6a6f4ca1cb774e55dcf6
Duncan McCracken <dcmccracken@me.com>
2019-03-09 06:20:20 +1100

    Update header.php

--- patchserver/var/www/webadmin/inc/header.php
+++ patchserver/var/www/webadmin/inc/header.php
@@ -12,57 +12,57 @@ $pageURI = $parts[count($parts) - 1];
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<meta http-equiv="refresh" content="<?php print ini_get("session.gc_maxlifetime"); ?>; url=/webadmin/logout.php">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta http-equiv="refresh" content="<?php print ini_get("session.gc_maxlifetime"); ?>; url=/webadmin/logout.php">
 
-	<title><?php echo (isset($title) ? $title : "Kinobi"); ?></title>
+		<title><?php echo (isset($title) ? $title : "Kinobi"); ?></title>
 
-	<!-- Bootstrap -->
-	<link href="theme/bootstrap.css" rel="stylesheet" media="all">
+		<!-- Bootstrap -->
+		<link href="theme/bootstrap.css" rel="stylesheet" media="all">
 
-	<!-- Roboto Font -->
-	<link href="theme/roboto.font.css" rel="stylesheet" type="text/css">
+		<!-- Roboto Font -->
+		<link href="theme/roboto.font.css" rel="stylesheet" type="text/css">
 
-    <!-- Custom styles for this project -->
-	<link href="theme/custom.css" rel="stylesheet" type="text/css">
+		<!-- Custom styles for this project -->
+		<link href="theme/custom.css" rel="stylesheet" type="text/css">
 
-	<!-- JQuery -->
-	<script type="text/javascript" src="scripts/jquery/jquery-2.2.4.js"></script>
+		<!-- JQuery -->
+		<script type="text/javascript" src="scripts/jquery/jquery-2.2.4.js"></script>
 
-	<!-- Bootstrap JavaScript -->
-	<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
-  </head>
+		<!-- Bootstrap JavaScript -->
+		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+	</head>
 
-  <body>
+	<body>
 
 	<!-- Fixed Top Navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" style="padding-top: 10px;" target="_blank" href="https://kinobi.io/"><img src="images/kinobi-logo-rev.svg" height="30"></a>
-          <div class="navbar-text">v1.2b5</div>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="<?php echo ($pageURI == "patchTitles.php" ? "active" : ""); ?>"><a href="patchTitles.php">Patch Definitions</a></li>
-            <li class="<?php echo ($pageURI == "patchSettings.php" ? "active" : ""); ?>"><a href="patchSettings.php">Settings</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="logout.php">Logout <?php echo (isset($_SESSION['username']) ? $_SESSION['username'] : ""); ?></a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" style="padding-top: 10px;" target="_blank" href="https://kinobi.io/"><img src="images/kinobi-logo-rev.svg" height="30"></a>
+				<div class="navbar-text">v1.2b8</div>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="<?php echo ($pageURI == "patchTitles.php" ? "active" : ""); ?>"><a href="patchTitles.php">Patch Definitions</a></li>
+					<li class="<?php echo ($pageURI == "patchSettings.php" ? "active" : ""); ?>"><a href="patchSettings.php">Settings</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="logout.php">Logout <?php echo (isset($_SESSION['username']) ? $_SESSION['username'] : ""); ?></a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</nav>
 
 	<div id="wrapper">
 
